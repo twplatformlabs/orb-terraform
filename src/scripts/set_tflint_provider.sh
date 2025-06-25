e9e7ce7768514766f98206bcdf8fc28909809735
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-06-25 11:07:29 -0500

    [nc] fix provider reference bug
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/set_tflint_provider.sh
+++ src/scripts/set_tflint_provider.sh
@@ -5,7 +5,7 @@ if [[ ! -f ".tflint.hcl" ]]; then
   ver=$(curl  "https://api.github.com/repos/terraform-linters/tflint-ruleset-$PROVIDER/tags" | jq -r '.[0].name' | grep -Eo '[0-9]\.[0-9]+\.[0-9]+')
   export ver
   cat <<EOF > .tflint.hcl
-plugin "aws" {
+plugin "$PROVIDER" {
     enabled = true
     version = "$ver"
     source  = "github.com/terraform-linters/tflint-ruleset-$PROVIDER"
