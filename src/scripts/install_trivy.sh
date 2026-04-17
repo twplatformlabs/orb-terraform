329d49b7105d86cdf611b1f39dea228159115e9f
Nic Cheneweth <nchenewe@thoughtworks.com>
2026-04-17 15:49:37 -0500

    [nc] modify trivy install, recheck snyk
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_trivy.sh
+++ src/scripts/install_trivy.sh
@@ -1,22 +1,11 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-# download trivy package and verification packages
-curl -SLO "https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSION}/trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz"
-curl -SLO "https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSION}/trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz.pem"
-curl -SLO "https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSION}/trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz.sig"
+# verify install version
+cosign verify aquasec/trivy:${TRIVY_VERSION} \
+--certificate-identity-regexp 'https://github\.com/aquasecurity/trivy/\.github/workflows/.+' \
+--certificate-oidc-issuer "https://token.actions.githubusercontent.com"
 
-# verify signature
-if result=$(cosign verify-blob "trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz" --certificate "trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz.pem" --signature "trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz.sig" --certificate-identity-regexp 'https://github\.com/aquasecurity/trivy/\.github/workflows/.+' --certificate-oidc-issuer "https://token.actions.githubusercontent.com" 2>&1); then
-    echo "Verified trivy installation package"
-else
-    echo "Unable to verify trivy installation package"
-    echo "cosign Output:"
-    echo "$result"
-    exit 1
-fi
-
-# install and smoke test trivy
-sudo tar -xzf "trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz"
-sudo mv trivy /usr/local/bin/trivy
+# install trivy version
+curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin v${TRIVY_VERSION}
 trivy --version
