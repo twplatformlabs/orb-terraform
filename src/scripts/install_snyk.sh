24457151fae2733b2c88b04f484e47857d1da53f
Nic Cheneweth <nchenewe@thoughtworks.com>
2026-04-17 15:59:48 -0500

    [nc] adjust snyk install
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_snyk.sh
+++ src/scripts/install_snyk.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 
 # download snyk and verification packages
-SNYK_SIGNING_KEY=A22665FB96CAB0E0973604C83676C4B8289C296E
+SNYK_SIGNING_KEY=467717A30B2B4658415975629691DA64D0025194   #A22665FB96CAB0E0973604C83676C4B8289C296E
 SNYK_PACKAGE_NAME=$(grep -q 'ID=alpine' /etc/os-release && echo "snyk-alpine" || echo "snyk-linux")
 curl -sSLO "https://static.snyk.io/cli/v${SNYK_VERSION}/${SNYK_PACKAGE_NAME}"
 curl -sSLO "https://static.snyk.io/cli/v${SNYK_VERSION}/sha256sums.txt.asc"
@@ -19,16 +19,15 @@ else
     exit 1
 fi
 
-# key server info no longer maintained this way apparently
-# # verify signature
-# if result=$(gpg --verify sha256sums.txt.asc  2>&1); then
-#     echo "Verified signature"
-# else
-#     echo "Unable to verify signature"
-#     echo "GPG Output:"
-#     echo "$result"
-#     exit 1
-# fi
+# verify signature
+if result=$(gpg --verify sha256sums.txt.asc  2>&1); then
+    echo "Verified signature"
+else
+    echo "Unable to verify signature"
+    echo "GPG Output:"
+    echo "$result"
+    exit 1
+fi
 
 # install and smoke test snyk
 sudo chmod +x "${SNYK_PACKAGE_NAME}"
