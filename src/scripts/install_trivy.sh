196800d5fd893f82c02211351e30eafc0864af8b
Nic Cheneweth <nchenewe@thoughtworks.com>
2026-04-17 15:52:22 -0500

    [nc] shellcheck issues in trivy install
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_trivy.sh
+++ src/scripts/install_trivy.sh
@@ -2,10 +2,10 @@
 set -eo pipefail
 
 # verify install version
-cosign verify aquasec/trivy:${TRIVY_VERSION} \
+cosign verify aquasec/trivy:"${TRIVY_VERSION}" \
 --certificate-identity-regexp 'https://github\.com/aquasecurity/trivy/\.github/workflows/.+' \
 --certificate-oidc-issuer "https://token.actions.githubusercontent.com"
 
 # install trivy version
-curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin v${TRIVY_VERSION}
+curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin v"${TRIVY_VERSION}"
 trivy --version
